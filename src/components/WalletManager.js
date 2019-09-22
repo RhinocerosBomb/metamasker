08758b1cfea487bbea1538b43a3ae3580a6b863e
Jason <rhinocerosbomb@gmail.com>
2019-09-21 21:21:50 -0400

    Styles and project folder reformat
    - Added background image
    - Added input styles
    - Styled "choose wallet"
    
    - Added components folder and moved all components there
    - moved all css into one file

---
+++ src/components/WalletManager.js
@@ -0,0 +1,56 @@
+import React, {useState, useContext} from 'react';
+import {ethers} from 'ethers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWallet, faTimes } from '@fortawesome/free-solid-svg-icons';
+import EthersContext from '../context/EthersContext';
+
+const defaultWalletFields = {name: '', privateKey: ''}
+function WalletManager({setWallets}) {
+  const [walletFields, setWalletFields] = useState(defaultWalletFields);
+  const {provider, wallets} = useContext(EthersContext);
+
+  const addWallet = () => {
+    if(ethers.utils.isHexString(walletFields.privateKey) &&
+    ethers.utils.hexDataLength(walletFields.privateKey) === 32 &&
+    !wallets.some(wallet => wallet.name === walletFields.name)
+  ) {
+      let wallet = new ethers.Wallet(walletFields.privateKey, provider);
+      setWallets(wallets.concat({name: walletFields.name, wallet}));
+      setWalletFields(defaultWalletFields);
+    }
+  }
+
+  const deleteWallet = (i) => {
+    setWallets([...wallets.slice(0,i), ...wallets.slice(i+1)]);
+  }
+
+  return (
+    <div className="walletManager">
+      <div className="walletFields">
+        <label>
+          Name:
+          <input type="text" value={walletFields.name} onChange={e => setWalletFields({...walletFields, name: e.target.value})}/>
+        </label>
+        <label>
+          Private Key:
+          <div className="inputBox">
+            <input type="text" value={walletFields.privateKey} onChange={e => setWalletFields({...walletFields, privateKey: e.target.value})}/>
+            <hr />
+          </div>
+        </label>
+        <button onClick={() => addWallet()}>Add</button>
+      </div>
+      <div className="wallets">
+      { wallets.map((wallet, i) => (
+        <div className="wallet" key={i}>
+          <span onClick={() => deleteWallet(i)}><FontAwesomeIcon icon={faTimes}/></span>
+          <FontAwesomeIcon icon={faWallet} size="lg"/>
+          {wallet.name}
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
+
+export default WalletManager;
