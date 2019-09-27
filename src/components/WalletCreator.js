5ffafd46c443277279d5c6775146e89e9ddada58
Jason <rhinocerosbomb@gmail.com>
2019-09-26 20:27:19 -0400

    Created WalletCreator
    - Separated the wallet maker to separate component in a modal. This gives more room for the wallets display

---
+++ src/components/WalletCreator.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect, useContext} from 'react';
+import EthersContext from '../context/EthersContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames';
+
+import Modal from './Modal';
+
+const defaultWalletFields = {name: '', privateKey: ''}
+
+function WalletChooser({addWallet, show, onClose }) {
+  const [walletFields, setWalletFields] = useState(defaultWalletFields);
+
+  const createWallet = () => {
+    addWallet(walletFields);
+    setWalletFields(defaultWalletFields);
+  }
+
+  return (
+      <Modal show={show} onClose={onClose}>
+        <div className="walletCreator">
+          <h3>Wallet Creator</h3>
+          <div className="walletFields">
+            <div className="inputBox walletField">
+              Name
+              <input type="text" value={walletFields.name} onChange={e => setWalletFields({...walletFields, name: e.target.value})}/>
+              <hr />
+            </div>
+            <div className="inputBox walletField">
+              Private Key
+              <input type="text" value={walletFields.privateKey} onChange={e => setWalletFields({...walletFields, privateKey: e.target.value})}/>
+              <hr />
+            </div>
+            <div className="addWalletBtn">
+              <button onClick={createWallet}>Add</button>
+            </div>
+          </div>
+        </div>
+      </Modal>
+  );
+}
+
+export default WalletChooser;
